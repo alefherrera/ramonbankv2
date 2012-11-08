cbce8b5ca989cd027dffbdf455459e2a950b65b3
alef.herrera <alef.herrera@d8ad714c-8f27-e710-eb7b-4d82f8fcc486>
2012-11-08 23:36:42 +0000

    muestro los clientes

--- RamonLabs/src/main/java/com/ramonlabs/ramonbank/action/ShowClientesAction.java
+++ RamonLabs/src/main/java/com/ramonlabs/ramonbank/action/ShowClientesAction.java
@@ -9,36 +9,34 @@ import utils.Contexto;
 import utils.OperationException;
 
 import java.util.List;
-import cliente.ClienteManager;
 
 import com.opensymphony.xwork2.ActionSupport;
 import com.ramonlabs.ramonbank.dbaccess.Cliente;
 
 public class ShowClientesAction extends ActionSupport implements SessionAware {
 
-	
+	private static final long serialVersionUID = 1L;
 	private List<Cliente> clientes = new ArrayList<Cliente>();
+
 	public List<Cliente> getClientes() {
-	return clientes;
-}
+		return clientes;
+	}
 
-public void setClientes(List<Cliente> clientes) {
-	this.clientes = clientes;
-}
+	public void setClientes(List<Cliente> clientes) {
+		this.clientes = clientes;
+	}
 
-	
-	
-public String execute() throws Exception, OperationException{
-		
-    	Cliente cliente = (Cliente) Contexto.getBean("clienteBean");
+	@Override
+	public String execute() throws Exception, OperationException {
+
+		Cliente cliente = (Cliente) Contexto.getBean("clienteBean");
 		setClientes(cliente.loadAll());
 		return SUCCESS;
 	}
-	
 
 	public void setSession(Map<String, Object> arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
